688d0bfd58eeb3be7bd937cc09394eab22b46614
katemcgeath <kate.mcgeath@ou.edu>
2023-05-15 11:05:34 -0500

    Restructured the clothes array into separate arrays specifying style.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /**Create global variables**/
 const clothes = [];
+
+var count;
 var selectedTops;
 var selectedDresses;
 var selectedBottoms;
 var selectedShoes;
 var selectedClothes;
+var selectedColor;
 var j = 0;
 var i = 0;
 var h = 0;
@@ -17,61 +20,81 @@ let values = [];
 let topExists = false;
 let bottomExists = false;
 let dressExists = false;
+let shoesExists = false;
 let checkboxes = 0;
 
 /**Create and Initialize clothes**/
-const short_sleeve1 = {img: "short-sleeve1.png", style: "short-sleeve", color: "grey", weather: ["summer", "spring", "fall"], type: "two-piece"};
-const short_sleeve2 = {img: "short-sleeve2.png", style: "short-sleeve", color: "blue", weather: ["summer", "spring", "fall"]};
-const short_sleeve3 = {img: "short-sleeve3.png", style: "short-sleeve", color: "white", weather: ["summer", "spring", "fall"]};
-const long_sleeve1 = {img: "long-sleeve1.png", style: "long-sleeve", color: "green", weather: ["winter", "spring", "fall"]};
-const long_sleeve2 = {img: "long-sleeve2.png", style: "long-sleeve", color: "blue", weather: ["winter", "spring", "fall"]};
-const long_sleeve3 = {img: "long-sleeve3.png", style: "long-sleeve", color: "black", weather: ["winter", "spring", "fall"]};
-const long_sleeve4 = {img: "long-sleeve4.png", style: "long-sleeve", color: "purple", weather: ["winter", "spring", "fall"]};
-const long_sleeve5 = {img: "long-sleeve5.png", style: "long-sleeve", color: ["grey", "black"], weather: ["winter", "spring", "fall"]};
-const skirt1 = {img: "skirt1.png", style: "skirt", color: "pink", weather: ["summer", "spring", "fall"]};
-const skirt2 = {img: "skirt2.png", style: "skirt", color: "black", weather: ["summer", "spring", "fall"]};
-const skirt3 = {img: "skirt3.png", style: "skirt", color: "pink", weather: ["summer", "spring", "fall"]};
-const skirt4 = {img: "skirt4.png", style: "skirt", color: "blue", weather: ["summer", "spring", "fall"]};
-const dress1 = {img: "dress1.png", style: "dress", color: "grey", weather: ["summer", "spring", "fall"]};
-const dress2 = {img: "dress2.png", style: "dress", color: ["green", "blue"], weather: ["summer", "spring", "fall"]};
-const dress3 = {img: "dress3.png", style: "dress", color: "green", weather: ["summer", "spring", "fall"]};
-const shoes1 = {img: "shoes1.png", style: "shoes", color: "black", weather: ["summer", "spring", "fall", "winter"]};
-const shoes2 = {img: "shoes2.png", style: "shoes", color: "red", weather: ["summer", "spring", "fall", "winter"]};
-const shoes3 = {img: "shoes3.png", style: "shoes", color: ["black", "grey"], weather: ["summer", "spring", "fall", "winter"]};
-const shorts1 = {img: "shorts1.png", style: "shorts", color: "blue", weather: ["summer", "spring", "fall"]};
-const shorts2 = {img: "shorts2.png", style: "shorts", color: "blue", weather: ["summer", "spring", "fall"]};
-const shorts3 = {img: "shorts3.png", style: "shorts", color: "blue", weather: ["summer", "spring", "fall"]};
-const shorts4 = {img: "shorts4.png", style: "shorts", color: "blue", weather: ["summer", "spring", "fall"]};
-const pants1 = {img: "pants1.png", style: "pants", color: "blue", weather: ["summer", "spring", "fall", "winter"]};
-const pants2 = {img: "pants2.png", style: "pants", color: "blue", weather: ["summer", "spring", "fall", "winter"]};
-const pants3 = {img: "pants3.png", style: "pants", color: "black", weather: ["summer", "spring", "fall", "winter"]};
 
-/**Put clothes objects into Clothes array**/
-clothes[0] = short_sleeve1;
-clothes[1] = short_sleeve2;
-clothes[2] = short_sleeve3;
-clothes[3] = long_sleeve1;
-clothes[4] = long_sleeve2;
-clothes[5] = long_sleeve3;
-clothes[6] = long_sleeve4;
-clothes[7] = long_sleeve5;
-clothes[8] = skirt1;
-clothes[9] = skirt2;
-clothes[10] = skirt3;
-clothes[11] = skirt4;
-clothes[12] = dress1;
-clothes[13] = dress2;
-clothes[14] = dress3;
-clothes[15] = shoes1;
-clothes[16] = shoes2;
-clothes[17] = shoes3;
-clothes[18] = shorts1;
-clothes[19] = shorts2;
-clothes[20] = shorts3;
-clothes[21] = shorts4;
-clothes[22] = pants1;
-clothes[23] = pants2;
-clothes[24] = pants3;
+const short_sleeves = [];
+short_sleeves[0] = {img: "short-sleeve1.png", style: "short-sleeve", color: "grey", weather: ["summer", "spring", "fall"], type: "two-piece"};
+short_sleeves[1] = {img: "short-sleeve2.png", style: "short-sleeve", color: "blue", weather: ["summer", "spring", "fall"]};
+short_sleeves[2] = {img: "short-sleeve3.png", style: "short-sleeve", color: "white", weather: ["summer", "spring", "fall"]};
+
+const long_sleeves = [];
+long_sleeves[0] = {img: "long-sleeve1.png", style: "long-sleeve", color: "green", weather: ["winter", "spring", "fall"]};
+long_sleeves[1] = {img: "long-sleeve2.png", style: "long-sleeve", color: "blue", weather: ["winter", "spring", "fall"]};
+long_sleeves[2] = {img: "long-sleeve3.png", style: "long-sleeve", color: "black", weather: ["winter", "spring", "fall"]};
+long_sleeves[3] = {img: "long-sleeve4.png", style: "long-sleeve", color: "purple", weather: ["winter", "spring", "fall"]};
+long_sleeves[4] = {img: "long-sleeve5.png", style: "long-sleeve", color: ["grey", "black"], weather: ["winter", "spring", "fall"]};
+
+const skirts = [];
+skirts[0] = {img: "skirt1.png", style: "skirt", color: "pink", weather: ["summer", "spring", "fall"]};
+skirts[1] = {img: "skirt2.png", style: "skirt", color: "black", weather: ["summer", "spring", "fall"]};
+skirts[2] = {img: "skirt3.png", style: "skirt", color: "pink", weather: ["summer", "spring", "fall"]};
+skirts[3] = {img: "skirt4.png", style: "skirt", color: "blue", weather: ["summer", "spring", "fall"]};
+
+const dresses = [];
+dresses[0] = {img: "dress1.png", style: "dress", color: "grey", weather: ["summer", "spring", "fall"]};
+dresses[1] = {img: "dress2.png", style: "dress", color: ["green", "blue"], weather: ["summer", "spring", "fall"]};
+dresses[2] = {img: "dress3.png", style: "dress", color: "green", weather: ["summer", "spring", "fall"]};
+dresses[3] = {img: "dress4.png", style: "dress", color: "purple", weather: ["summer", "spring", "fall"]};
+dresses[4] = {img: "dress5.png", style: "dress", color: "pink", weather: ["summer", "spring"]};
+dresses[5] = {img: "dress6.png", style: "dress", color: "orange", weather: ["summer", "spring"]};
+dresses[6] = {img: "dress7.png", style: "dress", color: "yellow", weather: ["summer", "spring", "fall"]};
+dresses[7] = {img: "dress8.png", style: "dress", color: "purple", weather: ["summer", "spring", "fall"]};
+dresses[8] = {img: "dress9.png", style: "dress", color: "brown", weather: ["summer", "spring", "fall"]};
+dresses[9] = {img: "dress10.png", style: "dress", color: "pink", weather: ["summer", "spring", "fall"]};
+dresses[10] = {img: "dress11.png", style: "dress", color: "black", weather: ["summer", "spring", "fall", "winter"]};
+dresses[11] = {img: "dress12.png", style: "dress", color: "pink", weather: ["summer", "spring", "fall"]};
+dresses[12] = {img: "dress13.png", style: "dress", color: "yellow", weather: ["summer", "spring", "fall"]};
+dresses[13] = {img: "dress14.png", style: "dress", color: "grey", weather: ["summer", "spring", "fall", "winter"]};
+dresses[14] = {img: "dress15.png", style: "dress", color: "red", weather: ["summer", "spring", "fall", "winter"]};
+dresses[15] = {img: "dress16.png", style: "dress", color: "blue", weather: ["summer", "spring", "fall", "winter"]};
+dresses[16] = {img: "dress17.png", style: "dress", color: "blue", weather: ["summer", "spring", "fall"]};
+dresses[17] = {img: "dress18.png", style: "dress", color: "blue", weather: ["summer", "spring", "fall"]};
+dresses[18] = {img: "dress19.png", style: "dress", color: "red", weather: ["summer", "spring", "fall"]};
+dresses[19] = {img: "dress20.png", style: "dress", color: "blue", weather: ["summer", "spring", "fall"]};
+dresses[20] = {img: "dress21.png", style: "dress", color: "blue", weather: ["summer", "spring", "fall", "winter"]};
+dresses[21] = {img: "dress22.png", style: "dress", color: "purple", weather: ["summer", "spring", "fall"]};
+
+const shoes = [];
+shoes[0] = {img: "shoes1.png", style: "shoes", color: "black", weather: ["summer", "spring", "fall", "winter"]};
+shoes[1] = {img: "shoes2.png", style: "shoes", color: "red", weather: ["summer", "spring", "fall", "winter"]};
+shoes[2] = {img: "shoes3.png", style: "shoes", color: ["black", "grey"], weather: ["summer", "spring", "fall", "winter"]};
+
+const shorts = [];
+shorts[0] = {img: "shorts1.png", style: "shorts", color: "blue", weather: ["summer", "spring", "fall"]};
+shorts[1] = {img: "shorts2.png", style: "shorts", color: "blue", weather: ["summer", "spring", "fall"]};
+shorts[2] = {img: "shorts3.png", style: "shorts", color: "blue", weather: ["summer", "spring", "fall"]};
+shorts[3] = {img: "shorts4.png", style: "shorts", color: "blue", weather: ["summer", "spring", "fall"]};
+
+const pants = [];
+pants[0] = {img: "pants1.png", style: "pants", color: "blue", weather: ["summer", "spring", "fall", "winter"]};
+pants[1] = {img: "pants2.png", style: "pants", color: "blue", weather: ["summer", "spring", "fall", "winter"]};
+pants[2] = {img: "pants3.png", style: "pants", color: "black", weather: ["summer", "spring", "fall", "winter"]};
+
+const overalls = [];
+overalls[0] = {img: "overalls1.png", style: "overalls", color: "black", weather: ["summer", "spring", "fall"]};
+overalls[1] = {img: "overalls2.png", style: "overalls", color: "blue", weather: ["summer", "spring", "fall"]};
+
+const rompers = [];
+rompers[0] = {img: "romper1.png", style: "romper", color: "white", weather: "summer"};
+rompers[1] = {img: "romper2.png", style: "romper", color: "white", weather: "summer"};
+rompers[2] = {img: "romper3.png", style: "romper", color: "black", weather: ["summer", "fall", "spring"]};
+rompers[3] = {img: "romper4.png", style: "romper", color: "grey", weather: ["summer", "fall", "spring", "winter"]};
+rompers[4] = {img: "romper5.png", style: "romper", color: "grey", weather: ["summer", "fall", "spring", "winter"]};
+rompers[5] = {img: "romper6.png", style: "romper", color: "pink", weather: ["summer", "fall", "spring", "winter"]};
+
 
 
 /**On Click event for each checkbox that is selected,
@@ -98,10 +121,12 @@ function getSelectedClothes() {
     topExists = false;
     bottomExists = false;
     dressExists = false;
+    shoesExists = false;
     selectedBottoms = [];
     selectedTops = [];
     selectedDresses = [];
     selectedShoes = []; 
+    selectedColor = [];
 
     //Pop off last value
     var val = values.pop();
@@ -110,104 +135,130 @@ function getSelectedClothes() {
     //If nothing is checked, use short-sleeve and shorts as template.
     if(val == undefined) {
 
-        i = 0;
-
         //Loop through the clothes array.
-        for(j = 0; j < clothes.length; j++) { 
+        for(j = 0; j < short_sleeves.length; j++) { 
 
-            //If object in clothes array is equal to short-sleeve,
             //add object image to selected tops array.
-            if(clothes[j].style == "short-sleeve") {
-                selectedTops.push(clothes[j].img);
+                selectedTops.push(short_sleeves[j].img);
                 topExists = true;
-                i++;
-            }
-            //If object in clothes array is equal to shorts,
+            
             //add object image to selected bottoms array.
-            if(clothes[j].style == "shorts") {
-                selectedBottoms.push(clothes[j].img);
+                selectedBottoms.push(shorts[j].img);
                 bottomExists = true;
-                i++;
-            }
+            
+            //add object image to selected shoes array.
+                selectedShoes.push(shoes[j].img);
+                shoesExists = true;
         }
     }
     else {
         //While val is defined, loop through clothes array.
         while(val) {
-            i = 0;
-            for(j = 0; j < clothes.length; j++) { 
 
-                //If clothes object's style is equal to the popped value,
-                if(clothes[j].style === val) {
+            //then compare value to short-sleeve.
+            //If val is equal, push clothes object onto selected tops array.
+            if(val === 'short-sleeve') {
+                selectedTops.push(short_sleeves[j].img);
+                topExists = true;
+            }
+            //then compare value to long-sleeve.
+            //If val is equal, push clothes object onto selected tops array.
+            if(val === 'long-sleeve') {
+                selectedTops.push(long_sleeves[j].img);
+                topExists = true;
+            }
+            //then compare value to shorts.
+            //If val is equal, push clothes object onto selected bottoms array.
+            if(val === 'shorts') {
+                selectedBottoms.push(shorts[j].img);
+                bottomExists = true;
+            }
+            //then compare value to pants.
+            //If val is equal, push clothes object onto selected bottoms array.
+            if(val === 'pants') {
+                selectedBottoms.push(pants[j].img);
+                bottomExists = true;
+            }
+            //then compare value to skirt.
+            //If val is equal, push clothes object onto selected bottoms array.
+            if(val === 'skirt') {
+                selectedBottoms.push(skirts[j].img);
+                bottomExists = true;
+            }
+            //then compare value to dress.
+            //If val is equal, push clothes object onto selected dresses array.
+            if(val === 'dress') {
+                selectedDresses.push(dresses[j].img);
+                dressExists = true;
+            }//then compare value to shoes.
+            //If val is equal, push clothes object onto selected shoes array.
+            if(val === 'shoes') {
+                selectedShoes.push(shoes[j].img);
+                shoesExists = true;
+            }
+                
+                // //If clothes object's color is equal to the popped value,
+                // if(clothes[j].color === val) {
 
-                    //then compare value to short-sleeve.
-                    //If val is equal, push clothes object onto selected tops array.
-                    if(val === 'short-sleeve') {
-                        selectedTops.push(clothes[j].img);
-                        topExists = true;
-                        i++;
-                    }
-                    //then compare value to long-sleeve.
-                    //If val is equal, push clothes object onto selected tops array.
-                    if(val === 'long-sleeve') {
-                        selectedTops.push(clothes[j].img);
-                        topExists = true;
-                        i++;
-                    }
-                    //then compare value to shorts.
-                    //If val is equal, push clothes object onto selected bottoms array.
-                    if(val === 'shorts') {
-                        selectedBottoms.push(clothes[j].img);
-                        bottomExists = true;
-                        i++;
-                    }
-                    //then compare value to pants.
-                    //If val is equal, push clothes object onto selected bottoms array.
-                    if(val === 'pants') {
-                        selectedBottoms.push(clothes[j].img);
-                        bottomExists = true;
-                        i++;
-                    }
-                    //then compare value to skirt.
-                    //If val is equal, push clothes object onto selected bottoms array.
-                    if(val === 'skirt') {
-                        selectedBottoms.push(clothes[j].img);
-                        bottomExists = true;
-                        i++;
-                    }
-                    //then compare value to dress.
-                    //If val is equal, push clothes object onto selected dresses array.
-                    if(val === 'dress') {
-                        selectedDresses.push(clothes[j].img);
-                        dressExists = true;
-                        i++;
-                    }
-                }
-            } 
+                //     //then compare value to black.
+                //     //If val is equal, push clothes object onto selected color array.
+                //     if(val === 'black') {
+                //         selectedColor.push(clothes[j].img);
+                //         i++;
+                //     }
+                //     //then compare value to white.
+                //     //If val is equal, push clothes object onto selected color array.
+                //     if(val === 'white') {
+                //         selectedColor.push(clothes[j].img);
+                //         i++;
+                //     }
+                //     //then compare value to grey.
+                //     //If val is equal, push clothes object onto selected color array.
+                //     if(val === 'grey') {
+                //         selectedColor.push(clothes[j].img);
+                //         i++;
+                //     }
+                //     //then compare value to blue.
+                //     //If val is equal, push clothes object onto selected color array.
+                //     if(val === 'blue') {
+                //         selectedColor.push(clothes[j].img);
+                //         i++;
+                //     }
+                //     //then compare value to green.
+                //     //If val is equal, push clothes object onto selected color array.
+                //     if(val === 'green') {
+                //         selectedColor.push(clothes[j].img);
+                //         i++;
+                //     }
+                //     //then compare value to red.
+                //     //If val is equal, push clothes object onto selected color array.
+                //     if(val === 'red') {
+                //         selectedColor.push(clothes[j].img);
+                //         i++;
+                //     }
+                //     //then compare value to pink.
+                //     //If val is equal, push clothes object onto selected color array.
+                //     if(val === 'pink') {
+                //         selectedColor.push(clothes[j].img);
+                //         i++;
+                //     }
+                // } 
+        }
         //Pop off the next value that is selected.
         val = values.pop();
-        }
-    }
+    }    
 }
 
 
 
 /**Get a random outfit from the selected clothes**/
 function getOutfit() {
-    
-    //If object in clothes array is equal to shoes,
-    //add object image to selected shoes array.
-    i = 0;
-    for(j = 0; j < clothes.length; j++) { 
-        if(clothes[j].style == "shoes") {
-            selectedShoes.push(clothes[j].img);
-            i++;
-        }
-    }
 
-    //Get random shoes.
-    var num0 = Math.floor( Math.random() * selectedShoes.length );
-    shoe = selectedShoes[ num0 ];
+    //If selected is shoes, get random shoes.
+    if(shoesExists) {
+        var num0 = Math.floor( Math.random() * selectedShoes.length );
+        shoe = selectedShoes[ num0 ];
+    }
     
     //If selected is a dress, get a random dress.
     if(dressExists) {
@@ -280,10 +331,18 @@ function displayOutfit() {
         document.getElementById("one-piece").src = imgDress;
     }
 
-    //Display random shoes.
-    var imgShoe = "";
-    imgShoe = "images/" + shoe;
-    document.getElementById("shoes").src = imgShoe;
+    //If shoes are selected, display them.
+    if(shoesExists) {
+
+        document.getElementById("shoes-div").style.display = "flex";
+
+        //Display random shoes.
+        var imgShoe = "images/" + shoe;
+        document.getElementById("shoes-img").src = imgShoe;
+    }
+    else {
+        document.getElementById("shoes-div").style.display = "none";
+    }
 }
 
 
